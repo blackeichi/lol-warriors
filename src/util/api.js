344ed9b5b6e39761c121a09a0861ff923f735282
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-10-04 20:43:27 +0900

    2022.10.04 Riot API가져오기 성공

---
+++ src/util/api.js
@@ -0,0 +1,10 @@
+const API_KEY = process.env.REACT_APP_API;
+const BASE_URL = "https://kr.api.riotgames.com/lol/";
+
+export function getPuuid(id) {
+  return fetch(
+    `${BASE_URL}summoner/v4/summoners/by-name/${id}?api_key=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .catch((e) => console.log(e));
+}
