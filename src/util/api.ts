d9808d120757f8805a072f75995ec20958ac48ee
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-10-04 20:56:47 +0900

    2022.10.04

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,7 +1,7 @@
 const API_KEY = process.env.REACT_APP_API;
 const BASE_URL = "https://kr.api.riotgames.com/lol/";
 
-export function getPuuid(id) {
+export function getPuuid(id: string) {
   return fetch(
     `${BASE_URL}summoner/v4/summoners/by-name/${id}?api_key=${API_KEY}`
   )
